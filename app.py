452daa3eb68dd29901fd9999628b977f031e8b92
Christopher Collier <christophercollier@wireless-10-147-30-58.public.utexas.edu>
2016-10-17 14:37:15 -0500

    Chris adds SQLAlchemy functions to app.py corresponding to pillars

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe'
 
 db = SQLAlchemy(app)
 manager = Manager(app)
-
+"""
 class Book(db.Model):
 	__tablename__ = 'books'
 	id = db.Column(db.Integer, primary_key=True)
@@ -29,27 +29,75 @@ class Author(db.Model):
 
 	def __repr__(self):
 		return '<Author %r>' % self.name
+"""
+
+
+class Game(db.Model):
+	__tablename__ = 'games'
+	#Column values are name, release date, genre, developers/publisher, rating of first release
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	release_date = db.Column(db.String)
+	genre = db.Column(db.String)
+	developers = db.Column(db.String)
+	rating = db.Column(db.Float)
+
+	def __repr__(self):
+		return '<Game>'
+
+class Platform(db.Model):
+	__tablename__ = 'platform'
+	#Column values are name, release date, company, starting price, number of sold units
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	release_date = db.Column(db.String)
+	company = db.Column(db.String)
+	starting_price = db.Column(db.String)
+	number_units_sold = db.Column(db.Integer)
+
+	def __repr__(self):
+		return '<Platform>'
+
+class Character(db.Model):
+	__tablename__ = 'characters'
+	#Column values are name, birthday, gender, deck, game first appeared in
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	birthday = db.Column(db.String)
+	gender = db.Column(db.String)
+	deck = db.Column(db.String)
+	game_first_appeared = db.Column(db.String)
+
+	def __repr__(self):
+		return '<Character>'
+
 
 @app.route('/')
 def index():
         return render_template('index.html')
 
-@app.route('/books/')
-def books():
-	b = Book.query.all()
-	return render_template('books.html', books=b)
+@app.route('/games/')
+def games():
+	b = Game.query.all()
+	return render_template('games.html', games=b)
 
-@app.route('/authors')
-def authors():
-	a = Author.query.all()
-	return render_template('authors.html', authors=a)
+@app.route('/platforms')
+def platforms():
+	a = Platform.query.all()
+	return render_template('platforms.html', platforms=a)
+
+@app.route('/characters')
+def characters():
+	c = Character.query.all()
+	return render_template('characters.html', characters=c)
 
 def shell_context():
 	context = {
 		'app': app,
 		'db': db,
-		'Book': Book,
-		'Author': Author
+		'Game': Game,
+		'Platforms':Platform,
+		'Characters':Characters
 	}
 	return context
 
