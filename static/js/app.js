cad0cf428cea431e8f61b980cccc8b42d63cb2f3
Eitan-Yarmush <eyarmush212@gmail.com>
2016-10-17 14:55:46 -0500

    Changed up app to include lists

--- static/js/app.js
+++ static/js/app.js
@@ -13,6 +13,15 @@ myApp.config(['$routeProvider',
              when('/about', {
                  templateUrl: '../static/partials/about.html',
              }).
+             when('/games', {
+                 templateUrl: '../static/partials/list.html',
+             }).
+             when('/platforms', {
+                 templateUrl: '../static/partials/list.html',
+             }).
+             when('/characters', {
+                 templateUrl: '../static/partials/list.html',
+             }).
              otherwise({
                  redirectTo: '/'
              });
