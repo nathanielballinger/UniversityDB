5423352be3258eb6fe836a2cfcec4c746059536f
abhirathod95 <abhirathod95@gmail.com>
2016-11-03 17:48:21 -0500

    removed unnecessary conflict leftovers

--- app/models.py
+++ app/models.py
@@ -14,11 +14,7 @@ app = Flask(__name__)
 #Nate's Database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bathory94@localhost:5432/swe'
 #Chris's database
-<<<<<<< HEAD
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe2'
-=======
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/swe2'
->>>>>>> be3a2dc5319fdd863c1c8467104ed70ecdb86d5a
 #Digital Ocean
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gusman772:MrSayanCanSing2@localhost:5432/swe'
 #Abhi's DB
