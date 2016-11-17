ee2995945501202c4cadbc3ca65d6923c6cdeeb7
abhirathod95 <abhirathod95@gmail.com>
2016-11-17 09:42:51 -0600

    FIxed controller reference

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -44,22 +44,6 @@ myApp.config(['$routeProvider',
              });
     }]);
 
-myApp.filter('highlight', function () {
-    return function (text, search, caseSensitive) {
-        if (text && (search || angular.isNumber(search))) {
-            text = text.toString();
-            search = search.toString();
-            if (caseSensitive) {
-                return text.split(search).join('<span class="ui-match">' + search + '</span>');
-            } else {
-                return text.replace(new RegExp(search, 'gi'), '<span class="ui-match">$&</span>');
-            }
-        } else {
-            return text;
-        }
-    };
-});
-
 myApp.filter('highlightWords', function () {
       return function (text, search) {
         if (text && (search || angular.isNumber(search))) {
