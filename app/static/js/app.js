d35f16d96eefb92daeca3c0206bd92799436b26e
abhirathod95 <abhirathod95@gmail.com>
2016-11-17 18:34:13 -0600

    Fixed search results

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -188,18 +188,9 @@ myApp.controller('papersCtrl', function($scope, $http, $location, $routeParams) 
 
 })
 
-function createKeywords(results) {
-    for(var i = 0; i < results.length; i++) {
-        var x = results[i].word_hits.join();
-        x = "Mario";
-        results[i].keywords = x;
-    }
-    return results;
-}
-
 myApp.controller('searchCtrl', function($scope, $http, $location, $cookieStore) {
-    $scope.sortType = "name";
-    $scope.sortReverse = false;
+    $scope.sortType = "type";
+    $scope.sortReverse = true;
     $scope.search = "";
 
     var data = $cookieStore.get('searchObj');
@@ -209,7 +200,10 @@ myApp.controller('searchCtrl', function($scope, $http, $location, $cookieStore) 
     console.log(data);
     $http.get("/search/result/" + data.pillar.toLowerCase() + "/" + data.string)
     .then(function (response) {
-        $scope.results = createKeywords(response.data);
+        console.log(response.data);
+        $scope.results = response.data.results;
+        console.log(response.data.search_text.split(" "));
+        $scope.keyWords = response.data.search_text.split();
         console.log($scope.results);          
     });
 
@@ -218,7 +212,9 @@ myApp.controller('searchCtrl', function($scope, $http, $location, $cookieStore) 
         var data = $cookieStore.get('searchObj');
         $http.get("/search/result/" + data.pillar.toLowerCase() + "/" + data.string)
         .then(function (response) {
-            $scope.results = createKeywords(response.data);
+            console.log(response.data);
+            $scope.results = response.data.results;
+            $scope.keyWords = response.data.search_text.split();
             console.log($scope.results);          
         });
     });
